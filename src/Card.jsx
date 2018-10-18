bd607e816db12c94703ff2cc408d902fc17f0aa8
Washington Soares <washingtonudi@gmail.com>
2018-10-18 14:10:45 -0300

    CSS improvements

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default ({ title, description, imageURL, learnMore }) => (
-  <div className="example-1 card">
+  <div className="example-1 card" onClick={() => window.open(learnMore, "_blank")}>
     <div
       className="wrapper"
       style={{ background: `url(${imageURL}) 50% 1% / cover no-repeat` }}
@@ -12,7 +12,6 @@ export default ({ title, description, imageURL, learnMore }) => (
             <a href="#">{title}</a>
           </h1>
           <p className="text">{description}</p>
-          <a href={learnMore} target="_blank">{title}</a>
         </div>
       </div>
     </div>
